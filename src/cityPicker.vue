e5d02f7d08c0db41e4c4aaabcad325983ee076a9
xiayan <xiayan@autohome.com.cn>
2018-12-06 18:17:08 +0800

    update citypicker.vue

--- src/cityPicker.vue
+++ src/cityPicker.vue
@@ -1,26 +1,26 @@
 <template>
-    <view style="display: flex;flex-direction: column;position: absolute; width: 100%; height: 100%;z-index: 5;">
-      <view style="flex: 9;background: rgba(75, 72, 72, 0.6)" @click.stop="cancelChangeCity" @catchtouchstart='cancelChangeCity' ></view>
-      <view style="width: 100%;flex: 8;background: #fff;display: flex;flex-direction: column;justify-content: space-between;">
-        <view class="bar">
-          <view class="bar-span" style="color:rgba(138,138,138,0.72);" @click.stop="cancelChangeCity">取消</view>
-          <view class="bar-span" style="color:rgb(26, 173, 25);" @click.stop="regionChangeCity">确定</view>
-        </view>
-        <picker-view :data-city="addressCode" :data-province="provinceCode" indicator-style="height: 68rpx;" style="width: 100%;height:440rpx;bottom: 0;"
+    <div style="display: flex;flex-direction: column;position: absolute; width: 100%; height: 100%;z-index: 5;">
+      <div style="flex: 9;background: rgba(75, 72, 72, 0.6)" @click.stop="cancelChangeCity" catchtouchstart='' catchtouchmove="handlerMove" ></div>
+      <div style="width: 100%;flex: 8;background: #fff;display: flex;flex-direction: column;justify-content: space-between;">
+        <div class="bar">
+          <div class="bar-span" style="color:rgba(138,138,138,0.72);" @click.stop="cancelChangeCity">取消</div>
+          <div class="bar-span" style="color:rgb(26, 173, 25);" @click.stop="regionChangeCity">确定</div>
+        </div>
+        <picker-view :data-city="addressCode" :data-province="provinceCode" indicator-style="height: 68rpx;" style="width: 100%;height:220px;bottom: 0;"
           :value="value" @change="bindChange">
           <picker-view-column>
-            <view v-for="(item, index) in provinces" :key="index" style="line-height: 68rpx;font-size:29rpx;text-align: center;color: black;">{{item.ProvinceName}}</view>
+            <div v-for="(item, index) in provinces" :key="index" style="line-height: 34px;font-size:29rpx;text-align: center;color: black;">{{item.ProvinceName}}</div>
           </picker-view-column>
-          <picker-view-column>
-            <view v-for="(item,index) in citys" :key="index" style="line-height: 68rpx;font-size:29rpx;text-align: center;color: black;">{{item.CityName}}</view>
+          <picker-view-column >
+            <div v-for="(item,index) in citys" :key="index" style="line-height: 34px;font-size:29rpx;text-align: center;color: black;">{{item.CityName}}</div>
           </picker-view-column>
         </picker-view>
-      </view>
-    </view>
+      </div>
+    </div>
 </template>
 
 <script>
-var cityProvinceCode = require('./cityProvinceCode')
+var cityProvinceCode = require('../utils/cityProvinceCode.js')
 
 const citys = []
 export default {
@@ -30,17 +30,18 @@ export default {
       value: [0, 0],
       city: '',
       province: '',
-      provinceIndex: 0
+      provinceIndex: 0,
+      provinceCode: ''
     }
   },
   name: 'citypicker',
   props: ['addressCode', 'provinces'],
   mounted () {
-    var codeStr = '' + this.addressCode
-    var code = codeStr.slice(0, 2) + '0000'
+    var cityid = this.addressCode.toString()
+    var code = cityid.substring(0, 2) + '0000'
     code = code === '0000' ? this.provinces[0].ProvinceId : code
     var provinceIndex = cityProvinceCode.getProvinceIndex(code)
-    var cityIndex = codeStr === '' ? 0 : cityProvinceCode.getCityIndex(code, codeStr)
+    var cityIndex = this.addressCode === '' ? 0 : cityProvinceCode.getCityIndex(code, cityid)
     this.provinceCode = code
     this.citys = cityProvinceCode.city[code]
     this.province = this.provinces[provinceIndex]
@@ -55,9 +56,11 @@ export default {
       }, {})
     },
     cancelChangeCity: function () {
+      console.log('cancelchange')
       this.$emit('cancelChange', {}, {})
     },
     bindChange: function (e) {
+      console.log(e)
       const val = e.mp.detail.value
       var province = this.provinces[val[0]]
       var citys = cityProvinceCode.city[province.ProvinceId]
@@ -79,14 +82,14 @@ export default {
 <style>
 .bar{
   display:flex;
-  border-bottom:1rpx solid #EEEEEE;
+  border-bottom:1px solid #EEEEEE;
   justify-content: space-between;
 }
 .bar-span{
-  font-size: 34rpx;
-  line-height: 90rpx;
-  padding-left:30rpx;
-  padding-right:30rpx;
+  font-size: 17px;
+  line-height: 45px;
+  padding-left:15px;
+  padding-right:15px;
 
 }
 </style>
