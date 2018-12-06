f8abab8b8910a1a3418997898bfb6bbf1d2881a8
xiayan <xiayan@autohome.com.cn>
2018-12-06 18:07:49 +0800

    update config

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
+// import Vue from 'vue'
 import App from './App'
+import VueTestCitypicker from './cityPicker.vue'
 
-Vue.config.productionTip = false
+export default VueTestCitypicker
+
+// Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+// new Vue({
+//   el: '#app',
+//   components: { App },
+//   template: '<App/>'
+// })
